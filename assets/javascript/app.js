209345367932fed585f1ce2b3513db245f6191f0
Bradley Knutson <bradley@bknutson.com>
2019-11-11 13:47:57 -0700

    Created user authentication and player creation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,70 @@ const firebaseConfig = {
     measurementId: "G-R3GNPGWDS5"
   };
 
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
 
-  $(document).ready(function () {
-    
-      
-  });
+var database = firebase.database();
+
+var userKey;
+var playerNum;
+
+//Upon connection, push a value to connectionsRef in order to generate a random key and use that to modify the userKey variable.
+//userKey becomes unique to the session running the game and thus different tabs can be used for different users.
+//Upon disconnect the user is removed from the connectionsRef
+database.ref('.info/connected').on('value', function(snapshot){
+    if(snapshot.val()){
+        var user = database.ref('connections').push(true);
+        userKey = user.getKey();
+        user.onDisconnect().remove();
+    }
+})
+
+
+//Upon entering a name in the input field, sets name equal to the value of it and sets the input to blank again.  Checks if the database has a player one and
+//if not will update accordingly, same for player two.  Sets both players to erase upon disconnect so another user can fill the spot.
+$('#nameInputSubmit').click(function(event){
+    event.preventDefault();
+    var name = $('#nameInput').val().trim();
+    $("#nameInput").val('');
+    database.ref('players').once('value', function(snapshot){
+        if(!snapshot.child('playerOne').exists()){
+            playerNum = 1;
+            database.ref('players/playerOne').set({
+                name: name,
+                wins: 0,
+                losses: 0,
+                key: userKey
+            })
+            database.ref('players/playerOne').onDisconnect().remove();
+        }else if(!snapshot.child('playerTwo').exists()){
+            playerNum = 2;
+            database.ref('players/playerTwo').set({
+                name: name,
+                wins: 0,
+                losses: 0,
+                key: userKey
+            })
+            database.ref('players/playerTwo').onDisconnect().remove();
+        }
+    })
+    $('#playerJoin').hide();
+})
+
+
+//display current player information
+database.ref('players').on('value', function(snapshot){
+    //Show join option only if there's an open spot
+    if((!snapshot.child('playerOne').exists() || !snapshot.child('playerTwo').exists()) && playerNum !== 1 && playerNum !== 2){
+        $('#playerJoin').show();
+    }
+    //Show playerOne info
+    $('#playerOneName').text(snapshot.child('playerOne/name').val());
+    $('#playerOneWins').text(snapshot.child('playerOne/wins').val());
+    $('#playerOneLosses').text(snapshot.child('playerOne/losses').val());
+    //Show playerTwo info
+    $('#playerTwoName').text(snapshot.child('playerTwo/name').val());
+    $('#playerTwoWins').text(snapshot.child('playerTwo/wins').val());
+    $('#playerTwoLosses').text(snapshot.child('playerTwo/losses').val());
+})
+
