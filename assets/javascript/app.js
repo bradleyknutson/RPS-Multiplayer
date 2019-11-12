2bdb770740404acdf2228225cc9e2d0b017fce8d
Bradley Knutson <bradley@bknutson.com>
2019-11-11 23:13:19 -0700

    Added more core gameplay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,15 +57,46 @@ $('#nameInputSubmit').click(function(event){
         }
     })
     $('#playerJoin').hide();
-})
+});
 
 
 //display current player information
 database.ref('players').on('value', function(snapshot){
+    showPlayerInfo(snapshot);
     //Show join option only if there's an open spot
     if((!snapshot.child('playerOne').exists() || !snapshot.child('playerTwo').exists()) && playerNum !== 1 && playerNum !== 2){
         $('#playerJoin').show();
     }
+    if(snapshot.child('playerOne').exists() && snapshot.child('playerTwo').exists()){
+        if(!snapshot.child('playerOne/choice').exists()){
+            $('#waiting').text('Waiting on Player One');
+        }else if(!snapshot.child('playerTwo/choice').exists()){
+            $('#waiting').text('Waiting on Player Two');
+        }else{
+            $('#waiting').text('');
+            determineWinner(snapshot);
+        }
+        showPlayerChoice(snapshot);
+    }
+    
+});
+
+
+$(document.body).on('click', '.rps-choice', function(){
+    var playerChoice = $(this).attr('value');
+    if(playerNum === 1){
+        database.ref('players/playerOne').update({
+            choice: playerChoice
+        });
+    }else if(playerNum === 2){
+        database.ref('players/playerTwo').update({
+            choice: playerChoice
+        });
+    }
+});
+
+
+function showPlayerInfo(snapshot){
     //Show playerOne info
     $('#playerOneName').text(snapshot.child('playerOne/name').val());
     $('#playerOneWins').text(snapshot.child('playerOne/wins').val());
@@ -74,5 +105,54 @@ database.ref('players').on('value', function(snapshot){
     $('#playerTwoName').text(snapshot.child('playerTwo/name').val());
     $('#playerTwoWins').text(snapshot.child('playerTwo/wins').val());
     $('#playerTwoLosses').text(snapshot.child('playerTwo/losses').val());
-})
+}
 
+//Builds the choice images for player
+function showPlayerChoice(snapshot){
+    if(playerNum === 1 && !snapshot.child('playerOne/choice').exists()){
+        $('#choiceSection').html('<div class="row"><div class="col-lg-4"><img src="assets/images/rock.png" alt="" value="rock" class="rps-choice"></div><div class="col-lg-4"><img src="assets/images/paper.png" alt="" value="paper" class="rps-choice"></div><div class="col-lg-4"><img src="assets/images/scissors.png" alt="" value="scissors" class="rps-choice"></div></div>');
+    }else if(playerNum === 2 && snapshot.child('playerOne/choice').exists() && !snapshot.child('playerTwo/choice').exists()){
+        $('#choiceSection').html('<div class="row"><div class="col-lg-4"><img src="assets/images/rock.png" alt="" value="rock" class="rps-choice"></div><div class="col-lg-4"><img src="assets/images/paper.png" alt="" value="paper" class="rps-choice"></div><div class="col-lg-4"><img src="assets/images/scissors.png" alt="" value="scissors" class="rps-choice"></div></div>');
+    }
+}
+
+function determineWinner(snapshot){
+    var playerOneChoice = snapshot.child('playerOne/choice').val();
+    var playerTwoChoice = snapshot.child('playerTwo/choice').val();
+    var playerOneWins = snapshot.child('playerOne/wins').val() + 1;
+    var playerTwoWins = snapshot.child('playerTwo/wins').val() + 1;
+    var playerOneLosses = snapshot.child('playerOne/losses').val() + 1;
+    var playerTwoLosses = snapshot.child('playerTwo/losses').val() + 1;
+
+    database.ref('players/playerOne/choice').remove();
+    database.ref('players/playerTwo/choice').remove();
+
+
+    if( (playerOneChoice === 'rock' && playerTwoChoice === 'scissors') ||
+        (playerOneChoice === 'paper' && playerTwoChoice === 'rock') ||
+        (playerOneChoice === 'scissors' && playerTwoChoice === 'paper')){
+            database.ref('players/playerOne').update({
+                wins: playerOneWins
+            });
+            database.ref('players/playerTwo').update({
+                losses: playerTwoLosses
+            });
+            $('#waiting').text('Player One Wins!');
+    }else if(playerOneChoice === playerTwoChoice){
+        $('#waiting').text("It's a tie!");
+    }else{
+        database.ref('players/playerTwo').update({
+            wins: playerTwoWins
+        });
+        database.ref('players/playerOne').update({
+            losses: playerOneLosses
+        });
+        $('#waiting').text('Player Two Wins!');
+    }
+    $('#choiceSection').empty();
+    if(playerNum === 1){
+        setTimeout(function(){
+            $('#choiceSection').html('<div class="row"><div class="col-lg-4"><img src="assets/images/rock.png" alt="" value="rock" class="rps-choice"></div><div class="col-lg-4"><img src="assets/images/paper.png" alt="" value="paper" class="rps-choice"></div><div class="col-lg-4"><img src="assets/images/scissors.png" alt="" value="scissors" class="rps-choice"></div></div>');
+        }, 1000 * 5);
+    }
+}
